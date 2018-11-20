b5e11d3dc61a9b699d01c55a1fbaa38450acd348
Michal Stachecki <michal.stachecki@gmail.com>
2018-11-20 11:04:31 +0100

    Add solution

---
+++ OpenFileConsole/OpenFileConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OpenFileConsole
+{
+    class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Multiselect = false
+            };
+
+            var openFileDialogResult = openFileDialog.ShowDialog();
+            if (openFileDialogResult == DialogResult.OK)
+            {
+                Console.WriteLine($"Opened file {openFileDialog.FileName}");
+            }
+            else
+            {
+                Console.WriteLine("File not selected");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
